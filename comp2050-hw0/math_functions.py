14cb6a305dbb9f1a7a2d0c906492eadfe4e52720
thangtahp <110162213+thangtahp@users.noreply.github.com>
2023-03-11 22:22:31 +0700

    Ex.1 done

--- comp2050-hw0/math_functions.py
+++ comp2050-hw0/math_functions.py
@@ -1,14 +1,35 @@
 """ File name:   math_functions.py
-    Author:      
-    Date:        
+    Author:      Ta Viet Thang
+    Date:        Mar 11, 2023
     Description: This file defines a set of variables and simple functions.
 
                  It should be implemented for Exercise 1 of Assignment 0.
 
                  See the assignment notes for a description of its contents.
 """
+import math
+ln_e = math.log(math.e)
 
-ln_e = # YOUR CODE HERE
+twenty_radians = 20*math.pi/180
 
-twenty_radians = # YOUR CODE HERE
+def quotient_ceil(numerator, denominator):
+    """ 
+    Returns the ceiling of the result of the division
+    (number, number) -> int
+    """
+    return int(math.ceil(numerator/denominator))
+
+def quotient_floor(numerator, denominator):
+    """
+    Returns the floor of the result of the division
+    (number, number) -> int
+    """
+    return int(math.floor(numerator/denominator))
+
+def manhattan(x1, y1, x2, y2):
+    """ 
+    Return the manhattan distance between two points (x1,y1) and (x2,y2)
+    """
+    return (abs(x1-x2)+abs(y1-y2))
+
 
